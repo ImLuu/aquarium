24d5a4fa168faac214e640372d11e73bf2f69ced
ImLuu <lmc018@alumnos.ucn.cl>
2023-06-03 13:21:38 -0400

    ningun cambio

---
+++ src/Components/Tarjet/Tarjets.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Tarjets.css'
+ function Tarjets() {
+  return (
+    <>
+        <div className='cartas'>
+            <div className='carta'> Tarjeta 1 </div>
+            <div className='carta'> Tarjeta 2 </div>
+            <div className='carta'> Tarjeta 3 </div>
+            <div className='carta'> Tarjeta 4 </div>
+        </div>
+    </>
+  )
+}
+export default Tarjets;
---
+++ src/Pages/Pagina/Inicio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Components/Head/Header';
+import Tarjets from '../../Components/Tarjet/Tarjets';
+
+ function Inicio() {
+  return (
+    <>
+        <Header/>
+        <Tarjets/>
+        
+    </>
+  )
+}
+export default Inicio;
